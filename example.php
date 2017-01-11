d90fe35a8db10263f498a1ded1256b3fead00c3c
Brad Berger <brad@bradb.net>
2017-01-11 10:01:57 +0100

    Adds example usage

--- example.php
+++ example.php
@@ -1,28 +1,10 @@
-[![Build Status](https://semaphoreci.com/api/v1/brad/taplink-php/branches/master/badge.svg)](https://semaphoreci.com/brad/taplink-php)
-[![codecov](https://codecov.io/gh/bradberger/taplink-php/branch/master/graph/badge.svg)](https://codecov.io/gh/bradberger/taplink-php)
-
-# PHP Bindings for Blind Hashing
-
-## Installation
-
-If you're using composer:
-
-```bash
-composer require taplink/taplink-php
-```
-
-Otherwise, include the `src/TapLink/TapLink/Client.php` and `src/TapLink/TapLink/Response.php` files.
-
-## Usage
-
-```php
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
 
 use \TapLink\TapLink\Client;
 
-$appId = "my-app-id";
+$appId = "7ddf60de9250dce2f9f9a4ff1f5be257eb42e81d872a9381271edddae1fb83f2f99b89f138354fb8098d1e9b6681d6b0a58bbd2b26637b545c1c32607e85d7cf";
 $taplink = new Client($appId);
 
 $hash = hash_hmac(Client::$hashAlgorithm, "secret", hex2bin("4cb78a1a60599df9c3bd9e4ac741a5f15feec1812b22a5f15bbad978039f2765f00dd82d97272eb3674cd164a0cc7024bbfd3704c6df6e2cb17a6562bd96ecb7"));
@@ -64,5 +46,3 @@ echo "Hash2Hex: {$res->hash2Hex}\n";
 echo "New version id: {$res->newVersionId}\n";
 echo "New Salt2Hex: {$res->newSalt2Hex}\n";
 echo "===================================\n";
-
-```
